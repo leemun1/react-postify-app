31efa1d2c517c3ef783e04f4cf6b5c45b1965097
Mike <munkyu0813@gmail.com>
2018-05-19 02:13:29 -0400

    setup eslint

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import AppRouter, { history } from './routers/AppRouter';
-import { login, logout } from './actions/auth';
-import configureStore from './store/configureStore';
 import 'normalize.css/normalize.css';
-import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
-import { firebase } from './firebase/firebase';
+import { login, logout } from './actions/auth';
 import LoadingPage from './components/LoadingPage';
+import AppRouter, { history } from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import { firebase } from './firebase/firebase';
+import './styles/styles.scss';
 
 const store = configureStore();
 
