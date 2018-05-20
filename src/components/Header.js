69c4756f3e487009f33a53b00469cf03a492f3f6
Mike <munkyu0813@gmail.com>
2018-05-20 00:06:55 -0400

    style page header

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,10 +7,18 @@ export const Header = ({ startLogout }) => (
   <header className="header">
     <div className="content-container">
       <div className="header__content">
-        <Link className="header__title" to="/dashboard">
-          <h1>Postify</h1>
-        </Link>
-        <button className="button button--link" onClick={startLogout}>Logout</button>
+        <div className="header__item">
+          <Link className="header__item__title" to="/dashboard">
+            <h1>Postify</h1>
+          </Link>
+        </div>
+        <div className="header__divider"></div>
+        <div className="header__item">
+          <Link className="header__item__title" to="/about">
+            <h3>About</h3>
+          </Link>
+          <button className="button button--link" onClick={startLogout}>Logout</button>
+        </div>
       </div>
     </div>
   </header>
